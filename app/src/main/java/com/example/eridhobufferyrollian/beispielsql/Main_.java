7c8bff5b0066324bb5f63f76e2da3172682904b9
eridhobffry <eridhobffry@yahoo.com>
2017-06-15 11:47:25 +0200

    activity main java

---
+++ app/src/main/java/com/example/eridhobufferyrollian/beispielsql/Main_.java
@@ -0,0 +1,11 @@
+package com.example.eridhobufferyrollian.beispielsql;
+
+/**
+ * Created by eridhobufferyrollian on 15.06.17.
+ */
+
+public class Main_ {
+    public Main_() {
+        super();
+    }
+}
