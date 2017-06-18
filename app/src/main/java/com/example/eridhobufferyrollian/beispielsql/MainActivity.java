4ff4489cc49ed8ac8f3932c60d15c3a1ed669f1f
eridhobffry <eridhobffry@yahoo.com>
2017-06-18 11:32:29 +0200

    Benutzer eingabe, onCreate -> onStart -> onResume -> onPause -> onStop, wird es in backsatck gelegt. GUI, onClickListener

--- app/src/main/java/com/example/eridhobufferyrollian/beispielsql/MainActivity.java
+++ app/src/main/java/com/example/eridhobufferyrollian/beispielsql/MainActivity.java
@@ -19,6 +19,14 @@ import android.widget.ListView;
 import java.util.List;
 
 
+
+import android.view.inputmethod.InputMethodManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -29,37 +37,105 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     //----------------  Man verbindet von hier zu der DBSource  -----------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //------------------------------------Test Dummies------------------------------------
-        DateiMemo testMemo = new DateiMemo("Wittman", "muster123", 1);
-        Log.d(LOG_TAG, "Inhalt der Testmemo: " + testMemo.toString());
-        //------------------------------------Test Dummies------------------------------------
-
-
-
+        Log.d(LOG_TAG, "Das Datenquellen-Objekt wird angelegt.");
         dataSource = new DateiMemoDbSource(this);
 
-        //anfangen, um zu verbinden
+        activateAddButton();
+
+
+//        //------------------------------------Test Dummies------------------------------------
+//
+//        DateiMemo testMemo = new DateiMemo("Wittman", "muster123", 1);
+//        Log.d(LOG_TAG, "Inhalt der Testmemo: " + testMemo.toString());
+//
+//
+//        //------------------------------------Test Dummies------------------------------------
+//
+//
+//
+//        dataSource = new DateiMemoDbSource(this);
+//
+//        //anfangen, um zu verbinden
+//        Log.d(LOG_TAG, "Die Datenquelle wird geöffnet.");
+//        dataSource.open();
+//
+//
+//        DateiMemo DateiMemo = dataSource.createDateiMemo("TestUser", "123123");
+//        Log.d(LOG_TAG, "Es wurde der folgende Eintrag in die Datenbank geschrieben:");
+//        Log.d(LOG_TAG, "ID: " + DateiMemo.getNid() + ", Inhalt: " + DateiMemo.toString());
+//
+//        Log.d(LOG_TAG, "Folgende Einträge sind in der Datenbank vorhanden:");
+//        showAllListEntries();
+//
+//        //schliessen
+//        Log.d(LOG_TAG, "Die Datenquelle wird geschlossen.");
+//        dataSource.close();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
         Log.d(LOG_TAG, "Die Datenquelle wird geöffnet.");
         dataSource.open();
 
-
-        DateiMemo DateiMemo = dataSource.createDateiMemo("TestUser", "123123", 2);
-        Log.d(LOG_TAG, "Es wurde der folgende Eintrag in die Datenbank geschrieben:");
-        Log.d(LOG_TAG, "ID: " + DateiMemo.getNid() + ", Inhalt: " + DateiMemo.toString());
-
         Log.d(LOG_TAG, "Folgende Einträge sind in der Datenbank vorhanden:");
         showAllListEntries();
+    }
 
-        //schliessen
+    @Override
+    protected void onPause() {
+        super.onPause();
+
         Log.d(LOG_TAG, "Die Datenquelle wird geschlossen.");
         dataSource.close();
+    }
+
+    private void activateAddButton() {
+        Button buttonAddUsername = (Button) findViewById(R.id.button_add_user);
+        final EditText editTextUsername = (EditText) findViewById(R.id.editText_username);
+        final EditText editTextPassword = (EditText) findViewById(R.id.editText_password);
+
+        buttonAddUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = editTextUsername.getText().toString();
+                String password = editTextUsername.getText().toString();
+
+                if(TextUtils.isEmpty(username)) {
+                    editTextUsername.setError(getString(R.string.editText_errorMessage));
+                    return;
+                }
+                if(TextUtils.isEmpty(password)) {
+                    editTextPassword.setError(getString(R.string.editText_errorMessage));
+                    return;
+                }
+
+
+                editTextUsername.setText("");
+                editTextPassword.setText("");
+
+                dataSource.createDateiMemo(username, password);
+
+                InputMethodManager inputMethodManager;
+                inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                if(getCurrentFocus() != null) {
+                    inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                }
+
+                showAllListEntries();
+            }
+        });
+
     }
     //----------------------------------------------------------------------------------------------
 //    @Override
