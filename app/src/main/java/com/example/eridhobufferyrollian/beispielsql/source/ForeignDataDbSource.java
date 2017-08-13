ef9af0643134bc90dfd6fea5d89b3088adc087e9
en <eridhobffry@yahoo.com>
2017-08-13 14:12:43 +0200

    import

--- app/src/main/java/com/example/eridhobufferyrollian/beispielsql/source/ForeignDataDbSource.java
+++ app/src/main/java/com/example/eridhobufferyrollian/beispielsql/source/ForeignDataDbSource.java
@@ -4,5 +4,19 @@ package com.example.eridhobufferyrollian.beispielsql.source;
  * Created by en on 13.08.17.
  */
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import com.example.eridhobufferyrollian.beispielsql.DateiMemoDbHelper;
+import com.example.eridhobufferyrollian.beispielsql.model.DateiMemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForeignDataDbSource {
 }
